9ebfdbd1a035ce2ec4d09b8c26d1eaf688a71db1
Anvar Kalykov <kalykov.anvar@gmail.com>
2021-06-21 22:41:35 +0600

    21062021 Bulls and Cows 1-7

---
+++ src/Stage1_7/BullsAndCows1_7.java
@@ -0,0 +1,53 @@
+package Stage1_7;
+
+import java.util.Scanner;
+
+public class BullsAndCows1_7 {
+    public static void main(String[] args) {
+        int a;
+        System.out.println("The secret code is prepared: ****.");
+        System.out.println();
+
+        System.out.println("Turn 1. Answer:");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(1234);
+        System.out.println("Grade: 1 cow.");
+        System.out.println();
+
+
+        System.out.println("Turn 2. Answer:");
+        System.out.println(5678);
+        System.out.println("Grade: 1 cow.");
+        System.out.println();
+
+
+        System.out.println("Turn 3. Answer:");
+        System.out.println(9012);
+        System.out.println("Grade: 1 bull and 1 cow.");
+        System.out.println();
+
+
+        System.out.println("Turn 4. Answer:");
+        System.out.println(9087);
+        System.out.println("Grade: 1 bull and 1 cow.");
+        System.out.println();
+
+
+        System.out.println("Turn 5. Answer:");
+        System.out.println(1087);
+        System.out.println("Grade: 1 cow.");
+        System.out.println();
+
+
+        System.out.println("Turn 6. Answer:");
+        System.out.println(9205);
+        System.out.println("Grade: 3 bulls.");
+        System.out.println();
+
+
+        System.out.println("Turn 7. Answer:");
+        System.out.println(9305);
+        System.out.println("Grade: 4 bulls.");
+        System.out.println("Congrats! The secret code is 9305.");
+    }
+}
